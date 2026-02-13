08f6d54abaa7a4dc9ad8503511c4923b7b2f30b0
sher95959696-hash <sher95959696@gmail.com>
2026-02-13 07:49:31 +0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,7 +5,7 @@ const config: CapacitorConfig = {
   // UNIQUE App ID for the Admin build. 
   // IMPORTANT: For User app, use 'com.barberpro.user'
   // For Admin app, use 'com.barberpro.admin'
-  appId: 'com.Razor's Edge Ltd.admin', 
+  appId: 'com.RazorsEdgeLtd.admin', 
   
   // Yahan jo naam likhenge, wahi mobile ki home screen pe icon ke neechy nazar ayega
   appName: 'Rezor's Admin Pannel', 
